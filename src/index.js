27e6f8769db72c5e51cf22bde1c91d18220d2c04
Leônidas Lima <leonidas.ollima@gmail.com>
2020-07-09 15:02:48 -0400

    disabled cors

--- src/index.js
+++ src/index.js
@@ -14,20 +14,6 @@ app.use(express.json())
 app.use(userRouter)
 app.use(pracaRouter)
 app.use(agendaRouter)
-//app.use(cors())
-app.use(cors({
-    origin: function (origin, callback) {
-        // allow requests with no origin 
-        // (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}));
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
