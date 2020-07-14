d9d3750de42cc50593837b05d07ec19284e204d4
Leônidas Lima <leonidas.ollima@gmail.com>
2020-07-14 15:03:43 -0400

    [added] sendgrid email

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 const express = require('express')
-const cors = require('cors')
-const app = express()
-
 require('./db/mongoose')
+
+// const cors = require('cors')
+
 const userRouter = require('./routers/user')
 const pracaRouter = require('./routers/praca')
 const agendaRouter = require('./routers/agenda')
 
-const port = 3001
+const app = express()
+const port =  process.env.PORT
 
-var whitelist = ['http://localhost:4200', 'https://agenda-pracas-app.herokuapp.com']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// var whitelist = ['http://localhost:4200', 'https://agenda-pracas-app.herokuapp.com']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(userRouter)
@@ -28,5 +29,5 @@ app.use(pracaRouter)
 app.use(agendaRouter)
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
+  console.log('Server is up on port ' + port)
 })
